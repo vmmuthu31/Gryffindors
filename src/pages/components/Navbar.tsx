0bcfba108d36aa1661278f1f3b6624142da86583
Vairamuthu <mvairamuthu2003@gmail.com>
2023-04-22 14:43:12 +0530

    updated the navbar section

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -3,14 +3,15 @@ import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { AiFillGithub, AiOutlineTwitter} from "react-icons/ai"
 import {FaDiscord, FaLinkedinIn} from "react-icons/fa"
+import Link from "next/link"
 
 
 const navigation = [
     { name: 'Home', href: '#' },
     { name: 'About', href: '#' },
     { name: 'Team', href: '#' },
-    { name: 'Products', href: '#' },
-    { name: 'Achievements', href: '#' },
+    { name: 'Products', href: 'Products' },
+    { name: 'Achievements', href: 'Achievements' },
     { name: 'Contact Us', href: '#' },
   ]
 
@@ -21,10 +22,10 @@ function Navbar() {
          <header className="absolute inset-x-0 top-0 z-50">
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
+            <Link href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
             <p className='text-3xl font-bold'>Gryffindors</p>
-            </a>
+            </Link>
           </div>
           <div className="flex lg:hidden">
             <button
@@ -38,9 +39,9 @@ function Navbar() {
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-md font-semibold leading-6 ">
+              <Link key={item.name} href={item.href} className="text-md font-semibold leading-6 ">
                 {item.name}
-              </a>
+              </Link>
             ))}
           </div>
           <div className="hidden text-2xl space-x-3 lg:flex lg:flex-1 lg:justify-end">
@@ -54,10 +55,10 @@ function Navbar() {
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
+              <Link href="/" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
                 <p className='text-xl font-bold'>Gryffindors</p>
-              </a>
+              </Link>
               <button
                 type="button"
                 className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -71,13 +72,13 @@ function Navbar() {
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="space-y-2 py-6">
                   {navigation.map((item) => (
-                    <a
+                    <Link
                       key={item.name}
                       href={item.href}
                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                     >
                       {item.name}
-                    </a>
+                    </Link>
                   ))}
                 </div>
                 <div className=" text-2xl space-x-3 flex  text-black lg:justify-end">
